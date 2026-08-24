5ed5616159e69ed4ac47e3a6d32fb27b1e89273f
katrinebruhn <katrinebruhn@icloud.com>
2026-08-24 14:28:05 +0200

    utils_lib.js

--- utils_lib/utils_lib.js
+++ utils_lib/utils_lib.js
@@ -1,3 +1,7 @@
 export function generateRandomNumBetween0AndMax(max) {
   return Math.floor(Math.random() * max);
 }
+
+export function $(element) {
+  return document.querySelector(elementName);
+}
