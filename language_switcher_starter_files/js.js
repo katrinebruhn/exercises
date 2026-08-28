d4999b861fa58831f5498d0930b5c6a77c0f7ba5
katrinebruhn <katrinebruhn@icloud.com>
2026-08-28 11:04:35 +0200

    language switcher

---
+++ language_switcher_starter_files/js.js
@@ -0,0 +1,18 @@
+const texts = {
+  de: {
+    texts: [
+      { text: "Das Bot", location: ".header" },
+      { text: "Das Ro-Bot", location: ".footer" },
+    ],
+  },
+  da: {
+    texts: [
+      { text: "Båden", location: ".header" },
+      { text: "Robotten", location: ".footer" },
+    ],
+  },
+};
+const locale = "da";
+console.log(texts[locale].texts[0].text);
+
+console.log(document.querySelector(".header"));
